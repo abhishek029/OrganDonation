2d43e314bdcf87aefb2e5fdc3096ad76a02569be
Your Name <jinkalp287@gmail.com>
2019-02-27 18:17:15 -0500

    home page component

--- js/main.js
+++ js/main.js
@@ -1,14 +1,40 @@
 (() => {
     const HomePageComponent = { 
         template: `
-        <section class="row">
-        <div class="col-sm-6 offset-sm-2">
-            <h2 class="col-sm-8">TWO MINUTES COULD SAVE EIGHT LIVES</h2>
-            <p class="col-sm-8">All you need is health card and a couple of minutes to become an organ donor. 
-                <a href="https://beadonor.ca/" target="_blank">Sign up now!</a>
-            </p>
+        <div>
+            <section id="promo" class="row">
+                <div class="col-sm-6 offset-sm-2" id="text">
+                    <h2 class="col-sm-8">TWO MINUTES COULD SAVE EIGHT LIVES</h2>
+                    <p class="col-sm-8">All you need is health card and a couple of minutes to become an organ donor. 
+                        <a href="https://beadonor.ca/" target="_blank">Sign up now!</a>
+                    </p>
+                </div>
+            </section>
+        
+            <section class="row" id="adds">
+                <div class="col-sm-12">
+                    <img class="col-sm-8 offset-sm-2" src="images/thumbnail.jpg" alt="Barbara Thomas">
+                    <p class="col-sm-6 offset-3 text-center"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos excepturi officia saepe blanditiis vero nostrum quae iusto possimus minus necessitatibus? Voluptatem ab esse animi veniam perspiciatis labore suscipit voluptates alias! All you need is health card and a couple of minutes to become an organ donor.</p>
+                </div>
+            </section>
+            
+            <section class="row" id="content">
+                <div class="col-sm-4">
+                    <img src="images/hourglass.svg" alt="Hourglass" class="col-sm-4 offset-sm-5 img-responsive">
+                    <p class="col-sm-6 offset-sm-4 text-center">4500 Canadains and 1500 ontarians are currently waiting for Organ Donation</p> 
+                </div>
+        
+                <div class="col-sm-4">
+                    <img src="images/heart.svg" alt="Heart" class="col-sm-4 offset-sm-5">
+                    <p class="col-sm-6 offset-sm-4 text-center">Your current or past medical history does not prevent you from registering to be a donor</p>                    
+                </div>
+                    
+                <div class="col-sm-4">
+                    <img src="images/eight.svg" alt="Eight" class="col-sm-4 offset-sm-5"> 
+                    <p class="col-sm-6 offset-sm-4 text-center">One donor can save up to 8 lives through donation and enhance even more through the gift pf tissue</p>                
+                </div>
+            </section>
         </div>
-        </section>
         `
     }
     
