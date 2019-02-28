fe4078c825b787a4d2ce87bf07d1f6112110b0ad
kylesmeltzer <kyle_smeltzer@hotmail.com>
2019-02-28 10:43:08 -0500

    updated promo css and links

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
                 <div class="col-sm-6 offset-sm-2" id="text">
                     <h2 class="col-sm-8">TWO MINUTES COULD SAVE EIGHT LIVES</h2>
                     <p class="col-sm-8">All you need is health card and a couple of minutes to become an organ donor. 
-                        <a href="https://beadonor.ca/" target="_blank">Sign up now!</a>
+                        <a href="https://www.ontario.ca/page/organ-and-tissue-donor-registration?utm_source=so&utm_medium=keyword&utm_campaign=original/" target="_blank">Sign up now!</a>
                     </p>
                 </div>
             </section>
